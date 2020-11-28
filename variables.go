4afcbc27a86c4ca6219887bb619e67fc2c6ebb9a
charles williams <charlesclinton2003@gmail.com>
2020-11-28 12:27:39 +0100

    done with variables

--- variables.go
+++ variables.go
@@ -3,9 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	var a = "initial"
-	print(a)
+	var a = "initial" // simple declaration and initializing of a single variable with var
+	fmt.Println(a)
 
-	var b, c int = 1, 2
+	var b, c int = 1, 2 // declaring and initializing multiple variables at once
 	fmt.Println(b, c)
+
+	var d = true
+	fmt.Println(d)
+
+	var e int // simple declararion of variables without initializing it, the come wuth zero values
+	fmt.Println(e)
+
+	f := "apple" // := is shorthand method of declaring and initializing variable
+	fmt.Println(f)
 }
